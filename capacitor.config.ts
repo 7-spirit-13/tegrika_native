b9b12b2ce2a7c2f328fa393e6a908c17e28cfa40
russian-proger <sagitov.2806@mail.ru>
2021-08-16 19:57:09 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tegrika_native',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
